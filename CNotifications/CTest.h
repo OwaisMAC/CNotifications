dc259ed100dc2e1ec50a5b14b084beaf6c3c2e04
Owais <owais.munawar@gmail.com>
2019-07-09 08:33:59 +0500

    Code pushed

---
+++ CNotifications/CTest.h
@@ -0,0 +1,32 @@
+//
+//  CTest.h
+//  CNotifications
+//
+//  Created by Owais Munawar on 3/20/19.
+//  Copyright © 2019 The Dev. All rights reserved.
+//
+
+#ifndef CTest_h
+#define CTest_h
+
+#include <stdio.h>
+
+/* Examples of declarations of functions */
+
+void square1(void);   /* Example of a function without input parameters
+                       and without return value */
+
+void square2(int i);  /* Example of a function with one input parameter
+                       and without return value */
+
+int square3(void);    /* Example of a function without input parameters
+                       and with integer return value */
+
+int square4(int i);   /* Example of a function with one input parameter
+                       and with integer return value */
+
+int area(int b, int h); /* Example of a function with two input parameters
+                         and with integer return value */
+int getPingValue(void);
+
+#endif /* CTest_h */
---
+++ CNotifications/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CNotifications
+//
+//  Created by Owais Munawar on 3/20/19.
+//  Copyright © 2019 The Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print(getPingValue())
+        print(square4(4))
+        print(getPingValue())
+        //print(pingValue)
+    }
+
+
+}
+
